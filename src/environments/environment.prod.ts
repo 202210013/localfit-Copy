fb64ee1cdf29a25c0d1d9068e290e754db275ae3
Garnnn <202210013@gordoncollege.edu.ph>
2025-11-29 20:42:30 +0800

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   // socketUrl: 'https://api.localfit.store'
 
 // Localhost URLs (use these for local development):
-  apiUrl: 'http://localhost/localfit%20-%20Copy/ecomm_api/',
-  imageBaseUrl: 'http://localhost/localfit%20-%20Copy/ecomm-images/',
+  apiUrl: 'http://localhost/localfit-Copy/ecomm_api/',
+  imageBaseUrl: 'http://localhost/localfit-Copy/ecomm-images/',
   socketUrl: 'http://localhost:3001'
 
 
